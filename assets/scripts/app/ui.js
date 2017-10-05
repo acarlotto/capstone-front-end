b1ee8da252bb192a68863ff66dc7894567064f17
acarlotto <acarlotto@cox.net>
2017-10-05 11:29:57 -0400

    last min changes

--- assets/scripts/app/ui.js
+++ assets/scripts/app/ui.js
@@ -125,7 +125,7 @@ const onSuccessGetAllListings = function (data) {
   const listings = data.listings
   //console.log('got all listings')
   listings.forEach (function (listing) {
-    $('#allListings').append('<div class="card"><img src=' + listing.imagelink + ' width="300px"><p class="card-text"> ' + listing.description + '</p><p> $' + listing.price + '0</p><p> ' + listing.paypalcode + '</p></div>')
+    $('#allListings').append('<div class="card"><h3>' + listing.name + '</h3><img src=' + listing.imagelink + ' width="300px"><p class="card-text"> ' + listing.description + '</p><p> $' + listing.price + '0</p><p> ' + listing.paypalcode + '</p></div>')
 
     })
 
